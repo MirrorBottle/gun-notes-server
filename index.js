964ac8bd774d0f50cfb857c616bebaea5fa7234d
MirrorBottle <setiawanbayu66152@gmail.com>
2021-08-21 22:23:14 +0800

    feat: add index.js

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const Gun = require('gun');
 const app = express();
 const notes = require("./api/notes");
+const index = require("./api/index");
 const port = process.env.PORT || 3030;
 
 app.use(Gun.serve);
+app.use("/", notes)
 app.use("/api/test", notes);
 const server = app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`);
